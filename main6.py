036e344aae54e39f2908afba61ca4f3b4eaa563e
jon759 <jon759@mtroyal.ca>
2026-09-22 20:11:02 -0600

    testing in main

--- main6.py
+++ main6.py
@@ -1,10 +1,12 @@
-from file_IO import load_from_html
+from file_IO import load_from_html, load_from_csv
 from data_processing import print_stats
 
 
 # load data
-filename = './A1_partial_solution/student_dataset.txt'
+filename = './data/student_dataset.txt'
+table_csv = load_from_csv('./data/census_dataset.txt')
 table = load_from_html(filename)
 
 # print table statistics
 print_stats(table)
+print_stats(table_csv)
