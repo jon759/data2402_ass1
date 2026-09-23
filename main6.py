5aa813487c1b3867dd85651d191c23704feb4f02
jon759 <jon759@mtroyal.ca>
2026-09-22 20:14:50 -0600

    tested corrupted student file

--- main6.py
+++ main6.py
@@ -4,9 +4,12 @@ from data_processing import print_stats
 
 # load data
 filename = './data/student_dataset.txt'
+filename_corr = './data/student_dataset_corrupted.txt'
 table_csv = load_from_csv('./data/census_dataset.txt')
 table = load_from_html(filename)
 
 # print table statistics
 print_stats(table)
 print_stats(table_csv)
+
+table = load_from_html(filename_corr)
