913ede72833928ef7fb32a6b16e2594d1dae065a
jon759 <jon759@mtroyal.ca>
2026-09-25 17:51:59 -0600

    edits

--- main6.py
+++ main6.py
@@ -6,8 +6,9 @@ from data_processing import print_stats
 filename = './data/student_dataset.txt'
 filename_corr = './data/student_dataset_corrupted.txt'
 table_html = load_from_data(filename)
-table_csv2 = load_from_data('./data/census_dataset.txt') 
-# table_csv = load_from_csv('./data/census_dataset.txt')
+# table_html_corr = load_from_data(filename_corr)
+table_csv2 = load_from_data('./data/census_subset.txt') 
+# table_csv = load_from_csv('./data/census_subset.txt')
 # table = load_from_html(filename)
 
 # print table statistics
@@ -16,4 +17,4 @@ print_stats(table_csv2)
 
 # table = load_from_html(filename_corr)
 save_to_json('student_data_json.txt', table_html)
-save_to_json('census_data_json.txt', table_csv2)
+save_to_json('census_subset_json.txt', table_csv2)
