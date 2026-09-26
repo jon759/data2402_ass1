2119de6f5737b60df2a4e2509c94fcbd1539c7b3
Ammar Ishtiaq <aisht038@mtroyal.ca>
2026-09-25 21:32:26 -0600

    main-work

--- main6_2.py
+++ main6_2.py
@@ -1,10 +1,16 @@
-from file_IO import load_from_html
-from data_processing import print_stats
+from file_IO_2 import load_from_html, load_from_csv, load_data, save_to_json
+from data_processing_2 import print_stats
 
 
-# load data
-filename = student_dataset.txt
-table = load_from_html(filename)
+filename = './data/student_dataset.txt'
+filename_corr = './data/student_dataset_corrupted.txt'
+table_html = load_data(filename)
+table_csv2 = load_data('./data/census_subset.txt') 
+
 
 # print table statistics
-print_stats(table)
+print_stats(table_html)
+print_stats(table_csv2)
+
+save_to_json('student_data_json.txt', table_html)
+save_to_json('census_subset_json.txt', table_csv2)
